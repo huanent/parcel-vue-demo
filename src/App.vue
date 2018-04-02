66cc47b0bfb9e32f86eadfe2e5fb9503a5666438
李凤焕 <huanent@hotmail.com>
2018-04-02 14:45:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <my-com></my-com>
+    </div>
+</template>
+
+<script>
+import MyCom from "./MyCom";
+export default {
+  components: {
+    MyCom
+  }
+};
+</script>
+
+<style>
+
+</style>
